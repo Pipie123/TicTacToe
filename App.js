ffc15d9c2a5a3d3e9c7e631b2b612fd5507f5079
Pipie123 <azw0074@auburn.edu>
2020-01-16 23:36:18 -0600

    Setup the Front Screen

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Animated, StyleSheet, Text, View, Image } from 'react-native';
+import {Button} from "react-native-web";
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>This is a test and we will see if this works</Text>
-    </View>
+      <View>
+        <View style = {styles.mainContainer}>
+          <View style = {styles.container}>
+            <Image
+              style = {styles.introImage}
+              source = {require('./assets/Tic.png')}
+            />
+          </View>
+          <View style = {styles.container1}>
+            <Image
+                style = {styles.introImage}
+                source = {require('./assets/Tac.png')}
+            />
+          </View>
+          <View style = {styles.container2}>
+            <Image
+                style = {styles.introImage}
+                source = {require('./assets/Toe.png')}
+            />
+          </View>
+        </View>
+      </View>
   );
 }
 
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    padding: 0,
+    margin: 0,
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 100,
+    maxHeight: 200,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container1: {
+    padding: 80,
+    maxHeight: 200,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    paddingBottom: 100,
+    maxHeight: 200,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  introImage: {
+    width: 375,
+    height: 125
+  },
 });
+
+
